55a656d2e32365bc12d2322ab0eb1c31dd1fac32
Shredhaya <shredhayagoel@gmail.com>
2025-12-02 21:26:56 +0530

    REACT STATE AND SETSTATE()

--- src/App.js
+++ src/App.js
@@ -1,60 +1,31 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-
 
 class App extends Component {
   constructor() {
     super();
-
-    this.state = { monsters: [1, 2], searchfield: 'a' };
-    console.log("before ", this.state);
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then((users) => this.setState({ monsters: users }))
-      .then((users) => {
-        console.log(users); // logs fetched data 
-        this.setState({ monsters: users }); // updates state properly
-      });
+    this.state = {
+      name: 'SHUBH'
+    };
   }
 
   render() {
-    const filtername = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchfield);
-    });
-
     return (
-      <div className="App">
-        <input className='search-box' type='search' placeholder='search-monsters' onChange={(event1) => {
-          console.log({ startingarray: this.state.monsters });
-
-          console.log(event1.target.value);
-
-          const searchfield = event1.target.value.toLocaleLowerCase();
-
-          this.setState(() => {
-            return { searchfield };
-          },
-            () => {
-              console.log({ endingarray: this.state.monsters });
-            }
-          );
-        }} />
-
-        {filtername.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
-
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Hi {this.state.name}</p>
+          <button onClick={() => {
+            //   this.state.name = ' SHUBH GOEL'
+            // this.setState(this.state);
+            this.setState({ name: 'SHUBH GOEL' });
+            console.log(this.state.name);
+          }}>Change Name</button>
+        </header>
       </div>
     );
   }
-
 }
 
 export default App;
