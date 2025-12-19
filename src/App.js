902827109fe3f2ade258b67c1e1a0f674a22291b
Shredhaya <shredhayagoel@gmail.com>
2025-12-19 12:26:38 +0530

    class components set state (creating objects for updating the value  and function for print in consle)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'SHUBH'
+      name: { firstname: 'Shubh', lastname: 'Goel' },
     };
   }
 
@@ -15,14 +15,29 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>Hi {this.state.name}</p>
+          <p>Hi {this.state.name.firstname} {this.state.name.lastname}</p>
           <button onClick={() => {
-            //   this.state.name = ' SHUBH GOEL'
-            // this.setState(this.state);
-            this.setState({ name: 'SHUBH GOEL' });
+
+            //   this.state.name = ' SHUBH GOEL'   // this.setState(this.state);
+            /*  this.setState({
+                name: { firstname: 'Shredhaya', lastname: 'Goel' },
+              });*/
+
             console.log(this.state.name);
-          }}>Change Name</button>
-        </header>
+            this.setState(
+              () => {
+                return {
+                  name: { firstname: 'Shredhaya', lastname: 'Goel' },
+                };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
+
+          }}
+          >Change Name</button>
+        </header>-
       </div>
     );
   }
