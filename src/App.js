1c9b240375a5b4b4a0d33b56b999ff27af9f80c9
Shredhaya <shredhayagoel@gmail.com>
2025-12-22 17:47:46 +0530

    SPA LIFECYCLE METHODS(COMPONENTDIDMOUNT) RENDER()  AND RE-RENDERS()

--- src/App.js
+++ src/App.js
@@ -4,27 +4,36 @@ import './App.css';
 
 class App extends Component {
   constructor() {
+    console.log('1');
+
     super();
     this.state = {
       name: { firstname: 'Shubh', lastname: 'Goel' },
       monsters: [
-        {
-          id: 1,
-          name: 'monster 1',
-        },
-        {
-          id: 2,
-          name: 'monster 2',
-        },
-        {
-          id: 3,
-          name: 'monster 3',
-        },
+
       ]
+
     };
   }
 
+  componentDidMount() {
+    console.log('3');
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          return { monsters: users };
+        },
+        () => {
+          console.log(this.state.monsters);
+        }
+      )
+      );
+  }
+
   render() {
+    console.log('2');
     return (
       <div className='App'>
         <header className='App-header'>
@@ -35,7 +44,7 @@ class App extends Component {
           })}
 
           <button onClick={() => {
-            
+
             console.log(this.state.name);
             this.setState(
               () => {
